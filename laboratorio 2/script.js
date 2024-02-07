73db4fc4e86e84ef9843e29da4fcb56f7adf126c
FrankUgb <111204154+FrankUgb@users.noreply.github.com>
2024-02-07 12:03:58 -0300

    Add files via upload

---
+++ laboratorio 2/script.js
@@ -0,0 +1,23 @@
+document.getElementById('clickExample1').addEventListener('click', function() {
+    alert('Haz hecho click en el Ejemplo 1');
+});
+
+document.getElementById('clickExample2').addEventListener('click', function() {
+    alert('Haz hecho click en el Ejemplo 2');
+});
+
+document.getElementById('clickExample3').addEventListener('click', function() {
+    alert('Haz hecho click en el Ejemplo 3');
+});
+
+document.getElementById('focusExample1').addEventListener('focus', function() {
+    alert('Estás enfocado en el Ejemplo 1');
+});
+
+document.getElementById('focusExample2').addEventListener('focus', function() {
+    alert('Estás enfocado en el Ejemplo 2');
+});
+
+document.getElementById('focusExample3').addEventListener('focus', function() {
+    alert('Estás enfocado en el Ejemplo 3');
+});
